bb719859b3b9b112c15c4d03234c325c0bd96c32
Faraz Fazlalizadeh <ffazlali@sfu.ca>
2019-09-17 21:06:18 -0700

    Initial Spring Boot Base Code

---
+++ src/main/java/com/cradlePlatform/webapi/WebapiApplication.java
@@ -0,0 +1,13 @@
+package com.cradlePlatform.webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebapiApplication.class, args);
+	}
+
+}
