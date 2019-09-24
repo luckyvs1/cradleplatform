30be3582c5d547c0dec60965f37661a3b12436af
Gurjovan Takhar <gurjovantakhar@gmail.com>
2019-09-23 19:48:37 -0700

    Added new base code with proper sql dependencies.
    Co-Authored-By: Linden Kyaw <lkyaw@sfu.ca>

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO User (id, username, password) VALUES ('1', 'adminuser', 'password123!');
+/*
+    For inserting initial data into tables.
+
+    Ex:
+    INSERT INTO User (id, username, password) VALUES ('1', 'adminuser', 'password123!');
+    INSERT INTO User (id, username, password) VALUES ('2', 'vhtuser', 'password123!');
+    INSERT INTO User (id, username, password) VALUES ('3', 'healthworkeruser', 'password123!');
+ */
