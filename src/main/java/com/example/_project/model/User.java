5d05a49a74a75cd95a242d41cc1d766f94f1e233
Gurjovan Takhar <gurjovantakhar@gmail.com>
2019-09-23 19:49:42 -0700

    Added user model and repo
    Co-Authored-By: Linden Kyaw <lkyaw@sfu.ca>

---
+++ src/main/java/com/example/_project/model/User.java
@@ -0,0 +1,53 @@
+package com.example._project.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "User")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private String id;
+    private String  username;
+    private String password;
+
+    public User(){
+    }
+
+    //User model
+    public User(String id, String username, String password){
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+}
