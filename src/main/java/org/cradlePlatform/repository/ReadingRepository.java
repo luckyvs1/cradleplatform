cf3f6c3e4d615d9bbbd8c6e7546be27d0dbc8b7e
nqhoang <nqhoang@sfu.ca>
2019-09-26 13:31:46 -0700

    Fix FollowUpController (id is auto-incremented) Add ReadingController.java and ReadingRepository.java

--- src/main/java/org/cradlePlatform/repository/ReadingRepository.java
+++ src/main/java/org/cradlePlatform/repository/ReadingRepository.java
@@ -4,7 +4,5 @@ import org.cradlePlatform.model.Reading;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ReadingRepository extends CrudRepository<Reading, String> {
-
+public interface ReadingRepository extends CrudRepository<Reading, Integer> {
 }
