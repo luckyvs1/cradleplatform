3f2b510f0470c122deb1fcdcd383e841fcde0ec0
lvs1 <lvs1@sfu.ca>
2019-09-26 11:41:48 -0700

    Add sample curl commands and update json payload format to fix types

--- src/main/java/org/cradlePlatform/controller/AndroidService.java
+++ src/main/java/org/cradlePlatform/controller/AndroidService.java
@@ -26,32 +26,38 @@ public class AndroidService {
                 " {\n" +
                 "  \"patientId\": \"1234\",\n" +
                 "  \"villageNumber\": \"12\",\n" +
-                "  \"initials\": \"ls\",\n" +
-                "  \"age\": \"72\",\n" +
+                "  \"initials\": \"ab\",\n" +
+                "  \"age\": \"32\",\n" +
                 "  \"gestationalAge\": {\n" +
-                "    \"age\": \"0\",\n" +
-                "    \"timeUnit\": \"1569483073\"\n" +
+                "    \"age\": \"12\",\n" +
+                "    \"timeUnit\": \"week\"\n" +
                 "  },\n" +
-                "  \"sex\": \"M\",\n" +
-                "  \"pregnant\": false,\n" +
-                "  \"timestamp\": \"1569483073\",\n" +
+                "  \"sex\": \"F\",\n" +
+                "  \"pregnant\": true,\n" +
+                "  \"timestamp\": \"2019-09-30 18:00:01\",\n" +
                 "  \"readings\": {\n" +
+                "    \"id\": \"1\",\n" +
                 "    \"symptoms\": {\n" +
                 "      \"noSymptoms\": true\n" +
+                "      \"headache\": false,\n" +
+                "      \"blurredVision\": false,\n" +
+                "      \"abdominalPain\": false,\n" +
+                "      \"feverish\": false,\n" +
+                "      \"unwell\": false\n" +
                 "    },\n" +
                 "    \"systolicBP\": \"70\",\n" +
                 "    \"diastolicBP\": \"85\",\n" +
                 "    \"pulseRate\": \"90\",\n" +
                 "    \"needFollowup\": false,\n" +
                 "    \"notes\": \"N/A\",\n" +
-                "    \"timeStamp\": \"1569483073\"\n" +
+                "    \"timeStamp\": \"2019-09-30 18:10:01\"\n" +
                 "  },\n" +
                 "  \"medications\": {\n" +
-                "    \"id\": \"100\",\n" +
+                "    \"id\": \"1\",\n" +
                 "    \"medicationName\": \"test\",\n" +
-                "    \"dosage\": \"once\",\n" +
-                "    \"startDate\": \"11/12/2019\",\n" +
-                "    \"endDate\": \"12/21/2019\"\n" +
+                "    \"dosage\": \"once a day\",\n" +
+                "    \"startDate\": \"2019-08-22\",\n" +
+                "    \"endDate\": \"2019-9-22\"\n" +
                 "  },\n" +
                 "  \"followUp\": {\n" +
                 "    \"id\": \"100\",\n" +
@@ -65,35 +71,41 @@ public class AndroidService {
                 " {\n" +
                 "  \"patientId\": \"5678\",\n" +
                 "  \"villageNumber\": \"12\",\n" +
-                "  \"initials\": \"ls\",\n" +
+                "  \"initials\": \"cd\",\n" +
                 "  \"age\": \"72\",\n" +
                 "  \"gestationalAge\": {\n" +
                 "    \"age\": \"0\",\n" +
-                "    \"timeUnit\": \"1569483073\"\n" +
+                "    \"timeUnit\": \"none\"\n" +
                 "  },\n" +
                 "  \"sex\": \"M\",\n" +
                 "  \"pregnant\": false,\n" +
-                "  \"timestamp\": \"1569483073\",\n" +
+                "  \"timestamp\": \"2019-09-30 18:20:01\",\n" +
                 "  \"readings\": {\n" +
+                "    \"id\": \"1\",\n" +
                 "    \"symptoms\": {\n" +
                 "      \"noSymptoms\": true\n" +
+                "      \"headache\": false,\n" +
+                "      \"blurredVision\": false,\n" +
+                "      \"abdominalPain\": false,\n" +
+                "      \"feverish\": false,\n" +
+                "      \"unwell\": false\n" +
                 "    },\n" +
                 "    \"systolicBP\": \"70\",\n" +
                 "    \"diastolicBP\": \"85\",\n" +
                 "    \"pulseRate\": \"90\",\n" +
                 "    \"needFollowup\": false,\n" +
                 "    \"notes\": \"N/A\",\n" +
-                "    \"timeStamp\": \"1569483073\"\n" +
+                "    \"timeStamp\": \"2019-09-30 18:40:01\"\n" +
                 "  },\n" +
                 "  \"medications\": {\n" +
-                "    \"id\": \"100\",\n" +
-                "    \"medicationName\": \"test\",\n" +
-                "    \"dosage\": \"once\",\n" +
-                "    \"startDate\": \"11/12/2019\",\n" +
-                "    \"endDate\": \"12/21/2019\"\n" +
+                "    \"id\": \"1\",\n" +
+                "    \"medicationName\": \"test2\",\n" +
+                "    \"dosage\": \"once a day\",\n" +
+                "    \"startDate\": \"2019-09-22\",\n" +
+                "    \"endDate\": \"2019-9-25\"\n" +
                 "  },\n" +
                 "  \"followUp\": {\n" +
-                "    \"id\": \"100\",\n" +
+                "    \"id\": \"1\",\n" +
                 "    \"required\": false,\n" +
                 "    \"frequency\": \"N/A\",\n" +
                 "    \"diagnosis\": \"Asthma\",\n" +
@@ -115,37 +127,43 @@ public class AndroidService {
     @ResponseStatus(HttpStatus.OK)
     public String mockPatient(@PathVariable String vhtId, @PathVariable String patientId) {
         return ("{\n" +
-                "  \"patientId\": \"1234\",\n" +
+                "  \"patientId\": \"5678\",\n" +
                 "  \"villageNumber\": \"12\",\n" +
-                "  \"initials\": \"ls\",\n" +
+                "  \"initials\": \"cd\",\n" +
                 "  \"age\": \"72\",\n" +
                 "  \"gestationalAge\": {\n" +
                 "    \"age\": \"0\",\n" +
-                "    \"timeUnit\": \"1569483073\"\n" +
+                "    \"timeUnit\": \"none\"\n" +
                 "  },\n" +
                 "  \"sex\": \"M\",\n" +
                 "  \"pregnant\": false,\n" +
-                "  \"timestamp\": \"1569483073\",\n" +
+                "  \"timestamp\": \"2019-09-30 18:00:01\",\n" +
                 "  \"readings\": {\n" +
+                "    \"id\": \"1\",\n" +
                 "    \"symptoms\": {\n" +
                 "      \"noSymptoms\": true\n" +
+                "      \"headache\": false,\n" +
+                "      \"blurredVision\": false,\n" +
+                "      \"abdominalPain\": false,\n" +
+                "      \"feverish\": false,\n" +
+                "      \"unwell\": false\n" +
                 "    },\n" +
                 "    \"systolicBP\": \"70\",\n" +
                 "    \"diastolicBP\": \"85\",\n" +
                 "    \"pulseRate\": \"90\",\n" +
                 "    \"needFollowup\": false,\n" +
                 "    \"notes\": \"N/A\",\n" +
-                "    \"timeStamp\": \"1569483073\"\n" +
+                "    \"timeStamp\": \"2019-09-30 18:40:01\"\n" +
                 "  },\n" +
                 "  \"medications\": {\n" +
-                "    \"id\": \"100\",\n" +
-                "    \"medicationName\": \"test\",\n" +
-                "    \"dosage\": \"once\",\n" +
-                "    \"startDate\": \"11/12/2019\",\n" +
-                "    \"endDate\": \"12/21/2019\"\n" +
+                "    \"id\": \"1\",\n" +
+                "    \"medicationName\": \"test2\",\n" +
+                "    \"dosage\": \"once a day\",\n" +
+                "    \"startDate\": \"2019-09-22\",\n" +
+                "    \"endDate\": \"2019-9-25\"\n" +
                 "  },\n" +
                 "  \"followUp\": {\n" +
-                "    \"id\": \"100\",\n" +
+                "    \"id\": \"1\",\n" +
                 "    \"required\": false,\n" +
                 "    \"frequency\": \"N/A\",\n" +
                 "    \"diagnosis\": \"Asthma\",\n" +
@@ -166,17 +184,23 @@ public class AndroidService {
     @ResponseStatus(HttpStatus.OK)
     public String mockReading(@PathVariable String vhtId, @PathVariable String patientId) {
         return ("{\n" +
-                "  \"patientId\": \"1234\",\n" +
                 "  \"readings\": {\n" +
+                "    \"id\": \"1\",\n" +
+                "    \"patientId\": \"1234\",\n" +
                 "    \"symptoms\": {\n" +
                 "      \"noSymptoms\": true\n" +
+                "      \"headache\": false,\n" +
+                "      \"blurredVision\": false,\n" +
+                "      \"abdominalPain\": false,\n" +
+                "      \"feverish\": false,\n" +
+                "      \"unwell\": false\n" +
                 "    },\n" +
                 "    \"systolicBP\": \"70\",\n" +
                 "    \"diastolicBP\": \"85\",\n" +
                 "    \"pulseRate\": \"90\",\n" +
                 "    \"needFollowup\": false,\n" +
                 "    \"notes\": \"N/A\",\n" +
-                "    \"timeStamp\": \"1569483073\"\n" +
+                "    \"timeStamp\": \"2019-09-30 18:40:01\"\n" +
                 "  },\n" +
                 "}");
     }
@@ -192,13 +216,13 @@ public class AndroidService {
     @ResponseStatus(HttpStatus.OK)
     public String mockMedication(@PathVariable String vhtId, @PathVariable String patientId) {
         return ("{\n" +
-                "  \"patientId\": \"1234\",\n" +
                 "  \"medications\": {\n" +
-                "    \"id\": \"100\",\n" +
+                "    \"id\": \"1\",\n" +
+                "    \"patientId\": \"1234\",\n" +
                 "    \"medicationName\": \"test\",\n" +
-                "    \"dosage\": \"once\",\n" +
-                "    \"startDate\": \"11/12/2019\",\n" +
-                "    \"endDate\": \"12/21/2019\"\n" +
+                "    \"dosage\": \"once a day\",\n" +
+                "    \"startDate\": \"2019-08-22\",\n" +
+                "    \"endDate\": \"2019-9-22\"\n" +
                 "  },\n" +
                 "}");
     }
@@ -214,9 +238,9 @@ public class AndroidService {
     @ResponseStatus(HttpStatus.OK)
     public String mockFollowUp(@PathVariable String vhtId, @PathVariable String patientId) {
         return ("{\n" +
-                "  \"patientId\": \"1234\",\n" +
                 "  \"followUp\": {\n" +
-                "    \"id\": \"100\",\n" +
+                "    \"id\": \"1\",\n" +
+                "    \"patientId\": \"1234\",\n" +
                 "    \"required\": false,\n" +
                 "    \"frequency\": \"N/A\",\n" +
                 "    \"diagnosis\": \"Asthma\",\n" +
