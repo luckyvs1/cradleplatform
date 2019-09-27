c2a762e10b63831c27595286cc2a50c0eb036fd0
nqhoang <nqhoang@sfu.ca>
2019-09-26 17:32:59 -0700

    Use java.sql.Timestamp instead of java.util.Date

--- src/main/java/org/cradlePlatform/controller/ReadingController.java
+++ src/main/java/org/cradlePlatform/controller/ReadingController.java
@@ -3,6 +3,7 @@
  */
 package org.cradlePlatform.controller;
 
+import java.sql.Timestamp;
 import java.util.Date;
 import org.cradlePlatform.model.Reading;
 import org.cradlePlatform.repository.ReadingRepository;
@@ -17,13 +18,13 @@ public class ReadingController {
     private ReadingRepository readingRepository;
 
     @PostMapping(path="/add")
-    public @ResponseBody String addAReading(@RequestParam String reader_id,
-                                            @RequestParam String patient_id,
-                                            @RequestParam String other_symptoms, @RequestParam int systolic_bp,
-                                            @RequestParam int diastolic_bp, @RequestParam int pulse_rate,
-                                            @RequestParam String notes, @RequestParam boolean need_followup){
+    public @ResponseBody String addAReading(@RequestParam String reader_id, @RequestParam String patient_id,
+                                            @RequestParam Timestamp timestamp, @RequestParam String other_symptoms,
+                                            @RequestParam int systolic_bp, @RequestParam int diastolic_bp,
+                                            @RequestParam int pulse_rate, @RequestParam String notes,
+                                            @RequestParam boolean need_followup){
         //TODO: not sure about arguments for symptoms
-        //TODO: add stuff here + maybe get current date/time for timestamp? - already imported the util for you, Jovan :)
+        //TODO: add stuff here
 
         return "Successfully saved";
     }
