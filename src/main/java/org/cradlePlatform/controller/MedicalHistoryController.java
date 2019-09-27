5ace38cfa417f4a2e06cd43f99457118f458d4c0
Gurjovan Takhar <gurjovantakhar@gmail.com>
2019-09-26 20:57:06 -0700

    added controllers for healthworker, medicalhisotry and added post for followup

---
+++ src/main/java/org/cradlePlatform/controller/MedicalHistoryController.java
@@ -0,0 +1,39 @@
+package org.cradlePlatform.controller;
+
+import org.cradlePlatform.model.MedicalHistory;
+import org.cradlePlatform.repository.MedicalHistoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping(path="/medicalhistory")
+public class MedicalHistoryController {
+    @Autowired
+    private MedicalHistoryRepository medicalHistoryRepository;
+
+    @PostMapping(path="/add")
+    public @ResponseBody String addMedicalHistory (@RequestParam int id, @RequestParam String patient_id,
+                                                   @RequestParam String med_history_text){
+        MedicalHistory newMedicalHistory = new MedicalHistory(patient_id, med_history_text);
+        newMedicalHistory.setPatientID(patient_id);
+        newMedicalHistory.setMedicalHistoryText(med_history_text);
+        newMedicalHistory.setId(id);
+        medicalHistoryRepository.save(newMedicalHistory);
+        return "Saved Medical History";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<MedicalHistory> getAllMedicalHistory(){
+        //This returns a JSON or XML with the users
+        return medicalHistoryRepository.findAll();
+    }
+
+    @GetMapping(path="/byId")
+    public @ResponseBody
+    Optional<MedicalHistory> getMedicalHistoryById(String patient_id){
+        return medicalHistoryRepository.findById(patient_id);
+    }
+}
