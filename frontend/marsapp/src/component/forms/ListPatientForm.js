27433456660862843c7144d74f974cecf84352a9
Faraz Fazlalizadeh <ffazlali@sfu.ca>
2019-09-27 08:39:46 -0700

    changed listing test info  issue: #11

--- frontend/marsapp/src/component/forms/ListPatientForm.js
+++ frontend/marsapp/src/component/forms/ListPatientForm.js
@@ -32,37 +32,37 @@ class ListPatientForm extends  React.Component {
                             <List.Item>
                                 <Image avatar src='https://react.semantic-ui.com/images/avatar/small/christian.jpg' />
                                 <List.Content>
-                                    <List.Header>Arron the MR.Robot HACKER</List.Header>
+                                    <List.Header>Test 1</List.Header>
                                 </List.Content>
                             </List.Item>
                             <List.Item>
                                 <Image avatar src='https://react.semantic-ui.com/images/avatar/small/helen.jpg' />
                                 <List.Content>
-                                    <List.Header>ALEX the CIA HACKER</List.Header>
+                                    <List.Header>Test 2</List.Header>
                                 </List.Content>
                             </List.Item>
                             <List.Item>
                                 <Image avatar src='https://react.semantic-ui.com/images/avatar/small/helen.jpg' />
                                 <List.Content>
-                                    <List.Header>JOVAN the FBI HACKER</List.Header>
+                                    <List.Header>Test 3</List.Header>
                                 </List.Content>
                             </List.Item>
                             <List.Item>
                                 <Image avatar src='https://react.semantic-ui.com/images/avatar/small/daniel.jpg' />
                                 <List.Content>
-                                    <List.Header>Gurinder the HARRY POTTER HACKER</List.Header>
+                                    <List.Header>Test 4</List.Header>
                                 </List.Content>
                             </List.Item>
                             <List.Item>
                                 <Image avatar src='https://react.semantic-ui.com/images/avatar/small/daniel.jpg' />
                                 <List.Content>
-                                    <List.Header>LUCKY the NASA HACKER</List.Header>
+                                    <List.Header>Test 5</List.Header>
                                 </List.Content>
                             </List.Item>
                             <List.Item>
                                 <Image avatar src='https://react.semantic-ui.com/images/avatar/small/helen.jpg' />
                                 <List.Content>
-                                    <List.Header>VICKY the GOOGLE HACKER</List.Header>
+                                    <List.Header>Test 6</List.Header>
                                 </List.Content>
                             </List.Item>
 
