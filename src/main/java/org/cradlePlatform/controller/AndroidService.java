3ffb08f43a9ed6c9c01c538139eb5135678f3d97
Aaron Cheung <ajc21@sfu.ca>
2019-09-26 23:39:48 -0700

    Add post mappings and update Reading class

--- src/main/java/org/cradlePlatform/controller/AndroidService.java
+++ src/main/java/org/cradlePlatform/controller/AndroidService.java
@@ -4,5 +4,33 @@
  */
 package org.cradlePlatform.controller;
 
+import org.cradlePlatform.model.ReadingUploadWrapper;
+import org.cradlePlatform.model.SyncDataWrapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import com.google.gson.Gson;
+
+@RestController
 public class AndroidService {
+
+
+	/**
+	 * Receive a single reading from a VHT's Cradle Android app.
+	 * @param reading The incoming reading.
+	 */
+	@PostMapping(path = "/api/readings", consumes = "application/json")
+	@ResponseStatus(HttpStatus.OK)
+	public void uploadReading(@RequestBody ReadingUploadWrapper reading) {
+		System.out.println(reading.getUsername() + " " + reading.getReading().getPatientId() + " " + reading.getReading().getGestationalAge().getTimeUnit());
+	}
+
+	/**
+	 * Receive patient, referral, and follow-up data from a VHT's Cradle Android app
+	 * during a server-app sync process.
+	 */
+	@PostMapping(path = "/api/sync", consumes = "application/json")
+	@ResponseStatus(HttpStatus.OK)
+	public void uploadSyncData(@RequestBody SyncDataWrapper data) {
+
+	}
 }
--- src/main/java/org/cradlePlatform/model/Reading.java
+++ src/main/java/org/cradlePlatform/model/Reading.java
@@ -1,4 +1,102 @@
+/**
+ * Reading class represents a
+ */
 package org.cradlePlatform.model;
 
+import java.sql.Date;
+
 public class Reading {
+	public enum Sex {MALE, FEMALE, OTHER}
+
+	// Patient info
+	String patientId;
+	String villageNumber;
+	String initials;
+	GestationalAge gestationalAge;
+	Sex sex;
+	boolean isPregnant;
+	Date date;
+	String symptoms;
+	int systolic;  // Blood pressure, top number
+	int diastolic;  // Blood pressure, bottom number
+	int pulseRate;
+	String notes;
+
+	public Reading() {
+
+	}
+
+	public Reading(String patientId,
+	               String villageNumber,
+	               String initials,
+	               GestationalAge gestationalAge,
+	               Sex sex,
+	               boolean isPregnant,
+	               Date date,
+	               String symptoms,
+	               int systolic,
+	               int diastolic,
+	               int pulseRate,
+	               String notes) {
+		this.patientId = patientId;
+		this.villageNumber = villageNumber;
+		this.initials = initials;
+		this.gestationalAge = gestationalAge;
+		this.sex = sex;
+		this.isPregnant = isPregnant;
+		this.date = date;
+		this.symptoms = symptoms;
+		this.systolic = systolic;
+		this.diastolic = diastolic;
+		this.pulseRate = pulseRate;
+		this.notes = notes;
+	}
+
+	public String getPatientId() {
+		return patientId;
+	}
+
+	public String getVillageNumber() {
+		return villageNumber;
+	}
+
+	public String getInitials() {
+		return initials;
+	}
+
+	public GestationalAge getGestationalAge() {
+		return gestationalAge;
+	}
+
+	public Sex getSex() {
+		return sex;
+	}
+
+	public boolean isPregnant() {
+		return isPregnant;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public String getSymptoms() {
+		return symptoms;
+	}
+
+	public int getSystolic() {
+		return systolic;
+	}
+
+	public int getDiastolic() {
+		return diastolic;
+	}
+
+	public int getPulseRate() {
+		return pulseRate;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
 }
