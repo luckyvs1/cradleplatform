a76429e4b63910ebafc5c7358d160ea90da2a53a
Faraz Fazlalizadeh <ffazlali@sfu.ca>
2019-09-27 23:57:34 -0700

    dynamic api calls and testing hello api issue: #11

--- frontend/marsapp/src/api.js
+++ frontend/marsapp/src/api.js
@@ -1,44 +1,50 @@
 import React, {Component} from 'react';
 import axios from "axios/dist/axios"
 
+
+
+const host = "localhost";
+const port = "8080";
+
+
 export default {
     user: {
-        hello: data => {
-            axios.get("/hello").then(res => console.log(res))
+        hello: () => {
+            axios.get(`http://${host}:${port}/hello`).then(res => console.log(res))
         },
-        login: data =>{
-            axios.post("/login", {}).then(res => console.log(res))
-        },
+
+        login: credential => axios.post(`http://${host}:${port}/login`, {credential}).then((res => res.data.user))
+,
     },
     patient: {
         mockPatients : data =>{
-            axios.get("/VHT/1/patients}", {}).then(res => console.log(res))
+            axios.get(`http://${host}:${port}/VHT/1/patients`, {}).then(res => console.log(res))
         },
         mockPatient: data => {
-            axios.get("/VHT/{1}/patients/{1}", {}).then(res => console.log(res))
+            axios.get(`http://${host}:${port}/VHT/{1}/patients/{1}`, {}).then(res => console.log(res))
         }
     },
 
     reading:{
         mockReading: data =>{
-            axios.get("/VHT/{vhtId}/patients/{patientId}/reading" , {}).then(res => console.log(res))
+            axios.get(`http://${host}:${port}/VHT/{vhtId}/patients/{patientId}/reading` , {}).then(res => console.log(res))
         },
         uploadReading: data =>{
-            axios.post("/api/readings" , {}).then(res => console.log(res))
+            axios.post(`http://${host}:${port}/api/readings` , {}).then(res => console.log(res))
         },
         uploadSyncData: data =>{
-            axios.post("/api/sync" , {}).then(res => console.log(res))
+            axios.post(`http://${host}:${port}/api/sync` , {}).then(res => console.log(res))
         },
 
     },
     medication:{
         mockMedication:data =>{
-            axios.get("/VHT/{vhtId}/patients/{patientId}/medication",{}).then(res => console.log(res))
+            axios.get(`http://${host}:${port}/VHT/{vhtId}/patients/{patientId}/medication`,{}).then(res => console.log(res))
         }
     },
     followUp: {
         mockFollowUp:data =>{
-            axios.get("/VHT/{vhtId}/patients/{patientId}/followup", {}).then(res => console.log(res))
+            axios.get(`http://${host}:${port}/VHT/{vhtId}/patients/{patientId}/followup`, {}).then(res => console.log(res))
         }
     }
 
