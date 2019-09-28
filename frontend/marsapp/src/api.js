d7c061d65b84878dbb59d49a57f5b422936e720e
Faraz Fazlalizadeh <ffazlali@sfu.ca>
2019-09-27 22:05:31 -0700

    api connection set up  issue: #11

--- frontend/marsapp/src/api.js
+++ frontend/marsapp/src/api.js
@@ -1,50 +1,44 @@
 import React, {Component} from 'react';
 import axios from "axios/dist/axios"
 
-
-
-const host = "localhost";
-const port = "8080";
-
-
 export default {
     user: {
-        hello: () => {
-            axios.get(`http://${host}:${port}/hello`).then(res => console.log(res))
+        hello: data => {
+            axios.get("/hello").then(res => console.log(res))
         },
-
-        login: credential => axios.post(`http://${host}:${port}/login`, {credential}).then((res => res.data.user))
-,
+        login: data =>{
+            axios.post("/login", {}).then(res => console.log(res))
+        },
     },
     patient: {
         mockPatients : data =>{
-            axios.get(`http://${host}:${port}/VHT/1/patients`, {}).then(res => console.log(res))
+            axios.get("/VHT/1/patients}", {}).then(res => console.log(res))
         },
         mockPatient: data => {
-            axios.get(`http://${host}:${port}/VHT/{1}/patients/{1}`, {}).then(res => console.log(res))
+            axios.get("/VHT/{1}/patients/{1}", {}).then(res => console.log(res))
         }
     },
 
     reading:{
         mockReading: data =>{
-            axios.get(`http://${host}:${port}/VHT/{vhtId}/patients/{patientId}/reading` , {}).then(res => console.log(res))
+            axios.get("/VHT/{vhtId}/patients/{patientId}/reading" , {}).then(res => console.log(res))
         },
         uploadReading: data =>{
-            axios.post(`http://${host}:${port}/api/readings` , {}).then(res => console.log(res))
+            axios.post("/api/readings" , {}).then(res => console.log(res))
         },
         uploadSyncData: data =>{
-            axios.post(`http://${host}:${port}/api/sync` , {}).then(res => console.log(res))
+            axios.post("/api/sync" , {}).then(res => console.log(res))
         },
 
     },
     medication:{
         mockMedication:data =>{
-            axios.get(`http://${host}:${port}/VHT/{vhtId}/patients/{patientId}/medication`,{}).then(res => console.log(res))
+            axios.get("/VHT/{vhtId}/patients/{patientId}/medication",{}).then(res => console.log(res))
         }
     },
     followUp: {
         mockFollowUp:data =>{
-            axios.get(`http://${host}:${port}/VHT/{vhtId}/patients/{patientId}/followup`, {}).then(res => console.log(res))
+            axios.get("/VHT/{vhtId}/patients/{patientId}/followup", {}).then(res => console.log(res))
         }
     }
 
--- frontend/marsapp/src/component/pages/LoginPage.js
+++ frontend/marsapp/src/component/pages/LoginPage.js
@@ -18,22 +18,26 @@ import {
 } from 'semantic-ui-react'
 
 class LoginPage extends React.Component {
-    submit = (data) =>
-        this.props.login(data).then(() => this.props.history.push("/")
-        );
+    submit = (data) => console.log("update information")
+
+    // this.props.login(data).then(() =>
+    //         this.props.history.push("/dashboard")
+    //     );
 
     render() {
         return (
-            <div className="ui-toolbar ">
-                <div className={"login"}>
-                    <h1 class="ui header">Login page</h1>
+            <div className="ui-toolbar">
+
+                <h1 class="ui header">Login page</h1>
 
 
-                    <LoginForm submit={this.submit}/>
+                        <LoginForm submit={this.submit}/>
+
+                        <Link to="/forgot_password">Forgot Password?</Link>
+                        <Link to="/signup">Sign Up?</Link>
+                        <Link className="ui-topsites" to="/homePage">GO TO HOME</Link>
 
 
-
-                </div>
             </div>
         );
     }
