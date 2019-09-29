29d019acea5fdb98471da37e9e80651b7ce785c0
nqhoang <nqhoang@sfu.ca>
2019-09-28 19:47:55 -0700

    Add "Add user" button and search bar Add clickable email address underneath user's name (previous commit works - User actually got to user. For some reason, the project didn't get updated with new changes. Thanks Vicky for checking my things!)

--- frontend/marsapp/src/component/forms/ListUserForm.js
+++ frontend/marsapp/src/component/forms/ListUserForm.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import {
     Button,
+    Icon,
     Checkbox,
     Form,
     Input,
@@ -28,41 +29,60 @@ class ListUserForm extends  React.Component {
                         <MenuTabularOnLeft></MenuTabularOnLeft>
                     </Grid.Column>
                     <Grid.Column width={14}>
+                        <Grid.Row>
+                            <Grid.Column floated='left'>
+                                <Button icon labelPosition='left'>
+                                    <Icon name='add circle' /> Add
+                                </Button>
+                            </Grid.Column>
+                            <Grid.Column floated='right'>
+                                <Input
+                                    icon={<Icon name='search'/>}
+                                    placeholder='Search user'
+                                />
+                            </Grid.Column>
+                        </Grid.Row>
                         <List selection verticalAlign='middle'>
                             <List.Item>
                                 <Image avatar src='https://react.semantic-ui.com/images/avatar/small/christian.jpg' />
                                 <List.Content>
                                     <List.Header>James Corden</List.Header>
+                                    <List.Description as='a'>james.corndog@gmail.com</List.Description>
                                 </List.Content>
                             </List.Item>
                             <List.Item>
                                 <Image avatar src='https://react.semantic-ui.com/images/avatar/small/daniel.jpg' />
                                 <List.Content>
                                     <List.Header>Conan OBrien</List.Header>
+                                    <List.Description as='a'>og_conan@gmail.com</List.Description>
                                 </List.Content>
                             </List.Item>
                             <List.Item>
                                 <Image avatar src='https://react.semantic-ui.com/images/avatar/small/helen.jpg' />
                                 <List.Content>
                                     <List.Header>Jimmy Fallon</List.Header>
+                                    <List.Description as='a'>sir_laugh_alot@gmail.com</List.Description>
                                 </List.Content>
                             </List.Item>
                             <List.Item>
                                 <Image avatar src='https://react.semantic-ui.com/images/avatar/small/daniel.jpg' />
                                 <List.Content>
                                     <List.Header>Jimmy Kimmel</List.Header>
+                                    <List.Description as='a'>kimmy_little@gmail.com</List.Description>
                                 </List.Content>
                             </List.Item>
                             <List.Item>
                                 <Image avatar src='https://react.semantic-ui.com/images/avatar/small/daniel.jpg' />
                                 <List.Content>
                                     <List.Header>Trevor Noah</List.Header>
+                                    <List.Description as='a'>noah_ark@gmail.com</List.Description>
                                 </List.Content>
                             </List.Item>
                             <List.Item>
                                 <Image avatar src='https://react.semantic-ui.com/images/avatar/small/helen.jpg' />
                                 <List.Content>
                                     <List.Header>Stephen Colbert</List.Header>
+                                    <List.Description as='a'>steve@gmail.com</List.Description>
                                 </List.Content>
                             </List.Item>
 
