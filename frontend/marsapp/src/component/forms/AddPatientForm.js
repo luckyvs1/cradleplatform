06cdb94c771f880b39e9e9aeacb631b729119099
gtakhar <gurjovantakhar@gmail.com>
2019-10-05 20:59:30 -0700

    Added add patient form

--- frontend/marsapp/src/component/forms/AddPatientForm.js
+++ frontend/marsapp/src/component/forms/AddPatientForm.js
@@ -5,80 +5,117 @@
  */
 
 import React from "react";
-import MenuTabularOnLeft from "../pages/MainMenu";
+import { Link } from "react-router-dom";
+import TopNavigation from "../navigation/TopNavigation";
 import {
+    Container,
+    Row,
+    Col,
     Button,
-    Checkbox,
-    Form,
-    Input,
-    Radio,
-    Select,
-    TextArea,
-    List,
-    Image,
-    Grid,
-} from 'semantic-ui-react'
-import HeaderMenu from "../pages/HeaderMenu";
-import {PageWrapper} from "../../wrappers/crd-page";
+    Form
+} from 'react-bootstrap';
 
 class AddPatientForm extends React.Component {
     // funcitons
-//    states
-    //submit
+    // states
+    // submit
     // validate
 
     render() {
         return (
-            <PageWrapper>
-                <h3>
-                    Add Patient
-                </h3>
-                <Grid.Column>
-                    <Form.Group grouped>
-                        <Form.Field
-                            control={Input}
-                            label='Patient ID'
-                            placeholder='Patient ID'
-                        />
-                        <Form.Field
-                            control={Input}
-                            label='Initials'
-                            placeholder='Initials'
-                        />
-                    </Form.Group>
-                </Grid.Column>
-                <Grid.Column>
-                    <Form.Group grouped>
-                        <Form.Field
-                            control={Input}
-                            label='Age'
-                            placeholder='Age'
-                        />
-                        <Form.Field
-                            control={Checkbox}
-                            label='Pregnant'
-                            placeholder='Pregnant'
-                        />
-                        <Form.Field
-                            control={Input}
-                            label='Gestational Age'
-                            placeholder='Gestational Age'
-                        />
-                    </Form.Group>
-                </Grid.Column>
-                <Grid>
-                    <Grid.Row>
-                        <Grid.Column width={10}>
-                        </Grid.Column>
-                        <Grid.Column widht={6}>
-                            <input type="submit" value="Submit"/>
-
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
-            </PageWrapper>
-
-
+            <div>
+                <TopNavigation authenticated={true}></TopNavigation>
+                <Container>
+                    <Row>
+                        <Col>
+                            <h1>Add Patient</h1>
+                            <hr></hr>
+                        </Col>
+                    </Row>
+                    <Form>
+                        <Row>
+                            <Col>
+                                <Form.Group>
+                                    <Form.Label>Patient ID</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        id="patient_id"
+                                        name="patient_id"
+                                        placeholder="Patient ID" />
+                                    {/*error handling*/}
+                                    {/* <Form.Text className="text-muted">
+                                        {errors.email && <InlineError text={errors.email} />}
+                                    </Form.Text> */}
+                                </Form.Group>
+                            </Col>
+                            <Col>
+                                <Form.Group>
+                                    <Form.Label>Initials</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        id="initials"
+                                        name="initials"
+                                        placeholder="Initials" />
+                                    {/*error handling*/}
+                                    {/* <Form.Text className="text-muted">
+                                        {errors.email && <InlineError text={errors.email} />}
+                                    </Form.Text> */}
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <Form.Group>
+                                    <Form.Label>Age</Form.Label>
+                                    <Form.Control
+                                        type="number"
+                                        id="age"
+                                        name="age"
+                                        placeholder="Age" />
+                                    {/*error handling*/}
+                                    {/* <Form.Text className="text-muted">
+                                        {errors.email && <InlineError text={errors.email} />}
+                                    </Form.Text> */}
+                                </Form.Group>
+                            </Col>
+                            <Col>
+                                <Form.Group>
+                                    <Form.Label>Pregnant</Form.Label>
+                                    <Form.Control as="select">
+                                        <option value={'true'}>Yes</option>
+                                        <option value={'False'}>No</option>
+                                    </Form.Control>
+                                    {/* enable his for error handling */}
+                                    {/* <Form.Text className="text-muted">
+                                        {errors.email && <InlineError text={errors.email} />}
+                                    </Form.Text> */}
+                                </Form.Group>
+                            </Col>
+                            <Col>
+                                <Form.Group>
+                                    <Form.Label>Gestational Age</Form.Label>
+                                    <Form.Control
+                                        type="number"
+                                        id="age"
+                                        name="age"
+                                        placeholder="Age" />
+                                    {/* enable his for error handling */}
+                                    {/* <Form.Text className="text-muted">
+                                        {errors.email && <InlineError text={errors.email} />}
+                                    </Form.Text> */}
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <Button variant="success" as={Link} to="listPatient">
+                                    Create
+                                </Button>
+                            </Col>
+                        </Row>
+                    </Form>
+                </Container>
+            </div>
         );
     }
 }
