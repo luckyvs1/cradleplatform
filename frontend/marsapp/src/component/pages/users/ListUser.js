26fa20b5b6191faa0666559626691b2c5183f2fb
gtakhar <gurjovantakhar@gmail.com>
2019-10-05 22:15:14 -0700

    Finished user list and help form

--- frontend/marsapp/src/component/pages/users/ListUser.js
+++ frontend/marsapp/src/component/pages/users/ListUser.js
@@ -8,26 +8,12 @@ import React from "react";
 import {connect} from "react-redux";
 import {testAPICALL} from "../../../actions/auth";
 
-import {
-    Button,
-    Checkbox,
-    Form,
-    Input,
-    Radio,
-    Select,
-    TextArea,
-    List,
-    Image,
-} from 'semantic-ui-react'
 import ListUserForm from "../../forms/ListUserForm";
 
 class ListUser extends React.Component {
     render() {
         return (
-            <div className="ui-toolbar">
-
-                <ListUserForm></ListUserForm>
-            </div>
+            <ListUserForm></ListUserForm>
         );
     }
 }
