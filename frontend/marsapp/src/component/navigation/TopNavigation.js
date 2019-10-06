0c472a0616e7e93117ce112508b4f0b1f22dcccf
gtakhar <gurjovantakhar@gmail.com>
2019-10-05 17:21:13 -0700

    Fixed dashboard tables

--- frontend/marsapp/src/component/navigation/TopNavigation.js
+++ frontend/marsapp/src/component/navigation/TopNavigation.js
@@ -18,25 +18,29 @@ import {
     Container
 } from 'react-bootstrap';
 
+const bottomMarginStyle = {
+    'margin-bottom': '40px'
+};
+
 class TopNavigation extends Component {
 
     render() {
         if (this.props.authenticated) {
             return (
-                <Navbar bg="dark" variant="dark">
+                <Navbar bg="dark" variant="dark" style={bottomMarginStyle}>
                     <Container>
                         <Navbar.Brand href="#home">Cradle Platform {this.props.authenticated}</Navbar.Brand>
                         <Nav className="mr-auto">
                             <Nav.Link as={Link} to="homePage">
-                                <i class="fas fa-tachometer-alt"></i> Dashboard
+                                <i className="fas fa-tachometer-alt"></i> Dashboard
                             </Nav.Link>
                             <Nav.Link as={Link} to="allFollowUp">
-                                <i class="fas fa-search"></i> Follow Ups
+                                <i className="fas fa-search"></i> Follow Ups
                             </Nav.Link>
                             <Nav.Link as={Link} to="referral">
-                                <i class="fas fa-redo"></i> Referrals
+                                <i className="fas fa-redo"></i> Referrals
                             </Nav.Link>
-                            <NavDropdown title={<span><i class="fas fa-users"></i> Patients</span>} id="collasible-nav-dropdown">
+                            <NavDropdown title={<span><i className="fas fa-users"></i> Patients</span>} id="collasible-nav-dropdown">
                                 <NavDropdown.Item as={Link} to="listPatient">All Patients</NavDropdown.Item>
                                 <NavDropdown.Item as={Link} to="patientDetail" href="#action/3.2">Find Patient</NavDropdown.Item>
                                 <NavDropdown.Divider />
@@ -45,22 +49,22 @@ class TopNavigation extends Component {
                         </Nav>
                         <Nav>
                             <Nav.Link as={Link} to="/homePage">
-                                <i class="fas fa-bell"></i>
+                                <i className="fas fa-bell"></i>
                             </Nav.Link>
-                            <NavDropdown title={<span><i class="fas fa-cogs"></i></span>} id="collasible-nav-dropdown">                                
+                            <NavDropdown title={<span><i className="fas fa-cogs"></i></span>} id="collasible-nav-dropdown">                                
                                 <NavDropdown.Item href="#action/3.2">
-                                    <i class="fas fa-user-alt"></i> Account
+                                    <i className="fas fa-user-alt"></i> Account
                                 </NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">
-                                    <i class="fas fa-users-cog"></i> Users
+                                    <i className="fas fa-users-cog"></i> Users
                                 </NavDropdown.Item>
                                 <NavDropdown.Divider />
                                 <NavDropdown.Item href="#action/3.3">
-                                    <i class="fas fa-graduation-cap"></i> Learning Materials
+                                    <i className="fas fa-graduation-cap"></i> Learning Materials
                                 </NavDropdown.Item>
                                 <NavDropdown.Divider />
                                 <NavDropdown.Item as={Link} to="/">
-                                    <i class="fas fa-sign-out-alt"></i> Logout
+                                    <i className="fas fa-sign-out-alt"></i> Logout
                                 </NavDropdown.Item>
                             </NavDropdown>
                         </Nav>                        
@@ -79,9 +83,9 @@ class TopNavigation extends Component {
 }
 
 TopNavigation.propTypes = {
-    user: PropTypes.shape({
-        email: PropTypes.string.isRequired
-    }).isRequired,
+    // user: PropTypes.shape({
+    //     email: PropTypes.string.isRequired
+    // }).isRequired,
     logout: PropTypes.func.isRequired
 };
 
--- frontend/marsapp/src/component/pages/HomePage.js
+++ frontend/marsapp/src/component/pages/HomePage.js
@@ -6,30 +6,22 @@
  */
 
 import React from "react";
-import PropTypes from "prop-types";
-import {Link} from "react-router-dom";
-import {connect} from "react-redux";
-import * as actions from "../../actions/auth";
-import Button from '@material-ui/core/Button';
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
-import Grow from '@material-ui/core/Grow';
-import Popper from '@material-ui/core/Popper';
-import MenuItem from '@material-ui/core/MenuItem';
-import MenuList from '@material-ui/core/MenuList';
-import "../../App.css"
-import {Grid, Image, List, Menu, Segment} from 'semantic-ui-react'
-import Upload from "./upload/PatientDetail";
-import {Route} from "react-router-dom";
-import {makeStyles} from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
+import { Link } from "react-router-dom";
+import { Grid } from 'semantic-ui-react'
+import { makeStyles } from '@material-ui/core/styles';
+// import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import MenuTabularOnLeft from "./MainMenu";
-import HeaderMenu from "./HeaderMenu";
 import TopNavigation from "../navigation/TopNavigation";
+import {
+    Container,
+    Row,
+    Col,
+    Table
+} from 'react-bootstrap';
 
 
 class FloatingMenuItem extends React.Component {
@@ -76,7 +68,7 @@ const rows = [
 ];
 
 class HomePage extends React.Component {
-    state = {activeItem: 'bio'}
+    state = { activeItem: 'bio' }
 
     constructor() {
         super();
@@ -87,11 +79,11 @@ class HomePage extends React.Component {
     }
 
     toggleMenu() {
-        this.setState({toggled: !this.state.toggled});
+        this.setState({ toggled: !this.state.toggled });
     }
 
 
-     useStyles = makeStyles(theme => ({
+    useStyles = makeStyles(theme => ({
         root: {
             width: '100%',
             marginTop: theme.spacing(2),
@@ -103,15 +95,15 @@ class HomePage extends React.Component {
         tableWrapper: {
             overflowX: 'auto',
         },
-         uiHeader: {
-            textAlign:'center',
-     }
+        uiHeader: {
+            textAlign: 'center',
+        }
 
     }));
 
     render() {
-        const {activeItem} = this.state
-        const useStyles= this.useStyles
+        const { activeItem } = this.state
+        const useStyles = this.useStyles
         let buttons = [];
         let className = "floating-menu";
         let icon = "add";
@@ -120,28 +112,97 @@ class HomePage extends React.Component {
             className += " open";
             icon = "clear";
             buttons.push(
-                <FloatingMenuItem label="Item 1" icon="create" action="" key="i1"/>);
+                <FloatingMenuItem label="Item 1" icon="create" action="" key="i1" />);
             buttons.push(
-                <FloatingMenuItem label="Short Cut 2" icon="drafts" action="" key="i2"/>);
+                <FloatingMenuItem label="Short Cut 2" icon="drafts" action="" key="i2" />);
 
         }
 
-        buttons.push(<FloatingMenuItem label="" icon={icon} action={this.toggleMenu.bind(this)} key="m"/>);
+        buttons.push(<FloatingMenuItem label="" icon={icon} action={this.toggleMenu.bind(this)} key="m" />);
 
 
         return (
-            <div className="ui-toolbar">
-                <div className="ui-toolbar">
+            <div>
                     <TopNavigation authenticated={true}></TopNavigation>
 
-                    {/* Bottom right hand controls don't know what they're used for */}
-                    {/* <div className="container">
-                        <div className={className}>
-                            {buttons}
-                        </div>
-                    </div> */}
+                    <Container>
+                        <Row>
+                            <Col>
+                                <Row>
+                                    <Col>
+                                        <h2>New Referrals</h2>
+                                        <hr></hr>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <Table bordered hover size="sm">
+                                            <thead>
+                                                <tr>
+                                                    <th>Patient ID</th>
+                                                    <th>Name</th>
+                                                    <th>Referred By</th>
+                                                    <th>Referral Date</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {rows.map(row => (
+                                                    <tr key={row.name}>
+                                                        <th scope="row">
+                                                            <Link to="referralDetail">
+                                                                {row.name}
+                                                            </Link>
+                                                        </th>
+                                                        <td>{row.calories}</td>
+                                                        <td>{row.fat}</td>
+                                                        <td>{row.carbs}</td>
+                                                    </tr>
+                                                ))}
+                                            </tbody>
+                                        </Table>
+                                    </Col>
+                                </Row>
+                            </Col>
+                            <Col>
+                                <Row>
+                                    <Col>
+                                        <h2>Upcoming Follow-ups</h2>
+                                        <hr></hr>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <Table bordered hover size="sm">
+                                            <thead>
+                                                <tr>
+                                                    <th>Patient ID</th>
+                                                    <th>Name</th>
+                                                    <th>Location</th>
+                                                    <th>Follow-up Date</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {rows.map(row => (
+                                                    <tr key={row.name}>
+                                                        <th scope="row">
+                                                            <Link to="referralDetail">
+                                                                {row.name}
+                                                            </Link>
+                                                        </th>
+                                                        <td>{row.calories}</td>
+                                                        <td>{row.fat}</td>
+                                                        <td>{row.carbs}</td>
+                                                    </tr>
+                                                ))}
+                                            </tbody>
+                                        </Table>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
+                    </Container>
 
-                    <Grid>
+                    {/* <Grid>
                         <Grid.Column width={3}>
                         </Grid.Column>
 
@@ -202,8 +263,7 @@ class HomePage extends React.Component {
 
 
                         </Grid.Column>
-                    </Grid>
-                </div>
+                    </Grid> */}
             </div>
 
         );
@@ -212,4 +272,3 @@ class HomePage extends React.Component {
 }
 
 export default HomePage;
-
