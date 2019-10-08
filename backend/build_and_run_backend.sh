f0b4311953bc77d409e98592944ca597a136e4b2
Aaron Cheung <ajc21@sfu.ca>
2019-10-08 00:12:34 -0700

    Add script to rebuild and run only backend

--- backend/build_and_run_backend.sh
+++ backend/build_and_run_backend.sh
@@ -1,10 +1,3 @@
-echo "Building database image..."
-docker build -t marsdb -f Dockerfile.marsdb .
-
-echo "Starting database and removing old database container."
-docker rm -f marssql
-docker run -d -p 3306:3306 --name marssql -e MYSQL_ROOT_PASSWORD=pass marsdb
-
 echo "Building backend server image..."
 docker build -t marsbackend -f Dockerfile.marsbackend .
 
