65f32fe1caa5c2d077449a3c7a4d24d24b73cc71
nqhoang <nqhoang@sfu.ca>
2019-10-09 05:14:00 -0700

    Modify ListPatientForm.js to read a JSON resource and store it into the component Change the file format to the build_and_run_backend_and_database.sh and gradlew to unix file format (the only way that works on my laptop) TODO: Fix "Blocked by CORS policy" issue

--- frontend/marsapp/src/component/forms/ListPatientForm.js
+++ frontend/marsapp/src/component/forms/ListPatientForm.js
@@ -21,64 +21,89 @@ class ListPatientForm extends React.Component {
     // states
     // submit
     // validate
+     //Getting data from the API and saving it inside of our app component (inside the state). The data
+        //is reusable in the component
+    constructor(props){
+            super(props);
+            this.state = {
+                items: [], //array of the data we're gonna fetch from the API
+                isLoaded: false,
+            }
+        }
+
+    componentDidMount(){
+        fetch('http://cmpt373.csil.sfu.ca:8084/VHT/1/patients/1', {mode: 'cors'})
+            .then(res => res.json())  //convert response from the API to JSON
+            .then(json => {
+                this.setState({
+                    isLoaded:true,
+                    items: json,
+                })
+            });
+    }
 
     render() {
-        return (
-            <div>
-                <TopNavigation authenticated={true}></TopNavigation>
-                <Container>
-                    <Row>
-                        <Col>
-                            <Row>
-                                <Col>
-                                    <h1>Patients</h1>
-                                </Col>
-                                <Col className="text-right">
-                                    <Button variant="primary" size="sm" as={Link} to="addPatient">
-                                        Create Patient
-                                    </Button>
-                                </Col>
-                            </Row>
-                            <hr></hr>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <Table bordered hover size="sm">
-                                <thead>
-                                    <tr>
-                                        <th width="10"></th>
-                                        <th>Name</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>
-                                            <Image src="https://react.semantic-ui.com/images/avatar/small/helen.jpg" rounded />
-                                        </td>
-                                        <td>
-                                            <Link to="patientDetail">
-                                                Test 1
-                                            </Link>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <Image src="https://react.semantic-ui.com/images/avatar/small/daniel.jpg" rounded />
-                                        </td>
-                                        <td>
-                                            <Link to="patientDetail">
-                                                Test 2
-                                            </Link>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </Table>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-        );
+        var { isLoaded, items } = this.state;
+        if(!isLoaded){
+            return <div>Loading...</div>
+        }else{
+            return (
+                <div>
+                    <TopNavigation authenticated={true}></TopNavigation>
+                    <Container>
+                        <Row>
+                            <Col>
+                                <Row>
+                                    <Col>
+                                        <h1>Patients</h1>
+                                    </Col>
+                                    <Col className="text-right">
+                                        <Button variant="primary" size="sm" as={Link} to="addPatient">
+                                            Create Patient
+                                        </Button>
+                                    </Col>
+                                </Row>
+                                <hr></hr>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <Table bordered hover size="sm">
+                                    <thead>
+                                        <tr>
+                                            <th width="10"></th>
+                                            <th>Name</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <Image src="https://react.semantic-ui.com/images/avatar/small/helen.jpg" rounded />
+                                            </td>
+                                            <td>
+                                                <Link to="patientDetail">
+                                                    Test 1
+                                                </Link>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <Image src="https://react.semantic-ui.com/images/avatar/small/daniel.jpg" rounded />
+                                            </td>
+                                            <td>
+                                                <Link to="patientDetail">
+                                                    Test 2
+                                                </Link>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </Table>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+            );
+        }
     }
 }
 
