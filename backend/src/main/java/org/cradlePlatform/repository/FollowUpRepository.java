905c96d42b2be28ed925cead7d9b2c4f9035b81e
Aaron Cheung <ajc21@sfu.ca>
2019-10-10 22:58:09 -0700

    Fix FollowUp stuff and change endpoint to use plural form

--- backend/src/main/java/org/cradlePlatform/repository/FollowUpRepository.java
+++ backend/src/main/java/org/cradlePlatform/repository/FollowUpRepository.java
@@ -4,6 +4,9 @@ import org.cradlePlatform.model.FollowUp;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FollowUpRepository extends CrudRepository<FollowUp, Integer> {
+	Iterable<FollowUp> findByPatientId(String patientId);
 }
