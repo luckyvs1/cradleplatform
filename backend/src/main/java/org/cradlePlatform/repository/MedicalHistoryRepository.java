55168d86796f2357e40c8b93779e193af86654dd
Aaron Cheung <ajc21@sfu.ca>
2019-10-14 02:18:17 -0700

    Update medicalhistorycontroller to use url params

--- backend/src/main/java/org/cradlePlatform/repository/MedicalHistoryRepository.java
+++ backend/src/main/java/org/cradlePlatform/repository/MedicalHistoryRepository.java
@@ -8,5 +8,6 @@ import java.util.Optional;
 
 @Repository
 public interface MedicalHistoryRepository extends CrudRepository<MedicalHistory, Integer> {
-	Optional<MedicalHistory> findTopByPatientIdOrderByIdDesc(String patientId);
+	Iterable<MedicalHistory> findTopByPatientIdOrderByIdDesc(String patientId);
+	Iterable<MedicalHistory> findAllByPatientId(String patientId);
 }
