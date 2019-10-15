4a97c0a3a559457c10aabf60e2f8421166dad642
lvs1 <lvs1@sfu.ca>
2019-10-15 13:02:03 -0700

    Update gestational age check constraint to check against 0 as primitive type can't be null
    Co-authored-by: Linden Kyaw <lkyaw@sfu.ca>

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -58,11 +58,12 @@ CREATE TABLE Patient (
     pregnant            BOOLEAN,
     gestational_start_date DATE           NOT NULL,
     gestational_age_unit  ENUM('week', 'month', 'none'),
-    current_gestational_age       INTEGER,
+    current_gestational_age       INTEGER   NOT NULL,
     CHECK (
             (pregnant IS TRUE AND current_gestational_age IS NOT NULL) OR
-            (pregnant IS FALSE AND current_gestational_age IS NULL)
+            (pregnant IS FALSE AND current_gestational_age = 0)
         ),
+
     CHECK (
             (attestation_no IS NOT NULL) OR
             (first_name IS NOT NULL AND last_name IS NOT NULL)
