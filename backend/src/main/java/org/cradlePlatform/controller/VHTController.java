e3858f64396307a60c8ad9784cddc93fe8c4ee6c
Faraz Fazlalizadeh <ffazlali@sfu.ca>
2019-10-15 23:19:53 -0700

    added cross origin for all controllers

--- backend/src/main/java/org/cradlePlatform/controller/VHTController.java
+++ backend/src/main/java/org/cradlePlatform/controller/VHTController.java
@@ -9,7 +9,7 @@ import org.cradlePlatform.repository.VHTRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
+@CrossOrigin(origins = { "http://localhost:3000"})
 @Controller
 @RequestMapping(path="/vht") //This means URL's start with /vht (after Application path)
 public class VHTController {
