be72a8a8808b7c94c5c0f4d790b9dbe810b9c431
nqhoang <nqhoang@sfu.ca>
2019-10-16 15:58:57 -0700

    Fix Patient.java from java.sql.Date back to java.util.Date because of @Temporal

--- backend/src/main/java/org/cradlePlatform/model/Patient.java
+++ backend/src/main/java/org/cradlePlatform/model/Patient.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.sql.Date;
+import java.util.Date;
 
 @Entity
 @Table(name = "Patient", schema = "schemas")
