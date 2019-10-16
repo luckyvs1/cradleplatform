f91f01afa0efa9c98a77129d0b261e832db41ce2
nqhoang <nqhoang@sfu.ca>
2019-10-16 15:53:44 -0700

    Fix spelling errors in Patient.java and update PatientController.java

--- backend/src/main/java/org/cradlePlatform/controller/PatientController.java
+++ backend/src/main/java/org/cradlePlatform/controller/PatientController.java
@@ -53,17 +53,19 @@ public class PatientController {
     @PostMapping(path="/api/patients")
     public @ResponseBody String addNewPatient (@RequestBody Patient patient){
         Patient newPatient = new Patient();
+        newPatient.setAttestationNo(patient.getAttestationNo());
+        newPatient.setFirstName(patient.getFirstName());
+        newPatient.setLastName(patient.getLastName());
         newPatient.setVillageNo(patient.getVillageNo());
-        newPatient.setAge(patient.getAge());
-        newPatient.setGestationAge(patient.getGestationAge());
-        newPatient.setGestationAgeUnit(patient.getGestationAgeUnit());
         newPatient.setInitials(patient.getInitials());
-        newPatient.setPregnant(patient.isPregnant());
-        newPatient.setVillageNo(patient.getVillageNo());
         newPatient.setSex(patient.getSex());
+        newPatient.setAge(patient.getAge());
+        newPatient.setPregnant(patient.isPregnant());
+        newPatient.setGestationalStartDate(patient.getGestationalStartDate());
+        newPatient.setGestationAgeUnit(patient.getGestationAgeUnit());
+        newPatient.setCurrentGestationalAge(patient.getCurrentGestationalAge());
         patientRepository.save(newPatient);
         return "Saved Patient";
-
     }
 
     /**
