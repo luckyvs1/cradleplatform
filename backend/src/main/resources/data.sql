57babedd74a21759b3a536813c12961b89f963c5
Faraz Fazlalizadeh <ffazlali@sfu.ca>
2019-10-16 22:45:15 -0700

    added sql command for adding new Patient and Follow up Rows

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -5,4 +5,14 @@
     INSERT INTO User (id, username, password) VALUES ('1', 'adminuser', 'password123!');
     INSERT INTO User (id, username, password) VALUES ('2', 'vhtuser', 'password123!');
     INSERT INTO User (id, username, password) VALUES ('3', 'healthworkeruser', 'password123!');
+
+
+    INSERT INTO Patient (id,attestation_no,first_name,last_name,village_no,zone_no,initials,sex,age,dob,pregnant,gestational_start_date,gestational_age_unit,current_gestational_age )
+    VALUES ("22","1234","bob","builder","1", "1","BB",'M', 22, "2019-1-1",true,"2019-1-1","week",1);
+ //follow up
+    INSERT INTO FollowUp (id,patient_id,notes,required,frequency,diagnosis,treatment)
+    VALUES (22,"22","Hello Im broken" ,true,"once a month","advil  every half an hour","trx 778");
  */
+
+
+
