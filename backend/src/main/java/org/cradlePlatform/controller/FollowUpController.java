084bf28d8c13251ba43f1c22206c136bdfa6e8fa
Aaron Cheung <ajc21@sfu.ca>
2019-10-18 23:36:04 -0700

    Add endpoint to get followup by id

--- backend/src/main/java/org/cradlePlatform/controller/FollowUpController.java
+++ backend/src/main/java/org/cradlePlatform/controller/FollowUpController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 
 @CrossOrigin(origins = { "http://localhost:3000"})
 @RestController
@@ -32,6 +34,11 @@ public class FollowUpController {
         } else {
             return followUpRepository.findByPatientId(patientId);
         }
+    }
+
+    @GetMapping(path="/api/followUps")
+    public Optional<FollowUp> getFollowUpByFollowUpId(@RequestParam int followUpId) {
+        return followUpRepository.findById(followUpId);
     }
 
     // POST mappings
