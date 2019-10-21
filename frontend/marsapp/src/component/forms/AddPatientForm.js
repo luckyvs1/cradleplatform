c625c9878e2f7f23fd38a62cc64c0a78bcb179f0
nqhoang <nqhoang@sfu.ca>
2019-10-20 21:09:08 -0700

    Add more fields to Add Patient page (refactoring - relate to #22) - Date Picker doesn't work (it does get the right date when printed through console, but the chosen date doesn't show up on the website) - No functionality just yet, basically a mockup

--- frontend/marsapp/src/component/forms/AddPatientForm.js
+++ frontend/marsapp/src/component/forms/AddPatientForm.js
@@ -6,6 +6,8 @@
 
 import React from "react";
 import { Link } from "react-router-dom";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import TopNavigation from "../navigation/TopNavigation";
 import {
     Container,
@@ -15,13 +17,59 @@ import {
     Form
 } from 'react-bootstrap';
 
-class AddPatientForm extends React.Component {
-    // funcitons
-    // states
-    // submit
-    // validate
 
+class AddPatientForm extends React.Component {
+    constructor(props){
+            super(props);
+            this.state = {
+                data:{
+                   attestation_no : "",
+                   first_name: "",
+                   last_name: "",
+                   village_no : "",
+                   zone_no : "",
+                   household_no : "",
+                   block_no : "",
+                   tank_no : "",
+                   initials : "",
+                   sex: "Other",
+                   age: 0,
+                   dob: new Date(),
+                   pregnant: "No",
+                   gestational_start_date: new Date(),
+                   gestational_age_unit: "none",
+                   current_gestational_age: 0},
+                isLoading: false,
+                errors: {}
+            };
+            this.onChange = this.onChange.bind(this);
+            this.onSubmit = this.onSubmit.bind(this);
+        }
+    onChange = e => this.setState({data: {...this.state.data, [e.target.name]: e.target.value} });
+    onChangeDob = date => {
+        this.setState({
+            dob: date
+        });
+        console.log(date);
+    };
+    onChangeDateGest = date => {
+        this.setState({
+            gestational_start_date: date
+        });
+        console.log(date);
+    };
+    onSubmit = (event) => {
+    /**
+        event.preventDefault();
+        const errors = this.validate(this.state.data);
+        this.setState({errors}); //if there are errors returned, display them
+        if(Object.keys(errors).length === 0){ //if not
+            this.props.submit(this.state.data);
+        }
+        */
+    };
     render() {
+        const { data, errors } = this.state;
         return (
             <div>
                 <TopNavigation authenticated={true}></TopNavigation>
@@ -35,83 +83,168 @@ class AddPatientForm extends React.Component {
                     <Form>
                         <Row>
                             <Col>
-                                <Form.Group>
-                                    <Form.Label>Patient ID</Form.Label>
-                                    <Form.Control
-                                        type="text"
-                                        id="patient_id"
-                                        name="patient_id"
-                                        placeholder="Patient ID" />
-                                    {/*error handling*/}
-                                    {/* <Form.Text className="text-muted">
-                                        {errors.email && <InlineError text={errors.email} />}
-                                    </Form.Text> */}
-                                </Form.Group>
+                                <Form.Row>
+                                    <Form.Group as={Col}>
+                                        <Form.Label>First Name</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            id="first_name"
+                                            name="first_name"
+                                            placeholder="Enter here..."
+                                            value={data.first_name}
+                                            onChange={this.onChange}/>
+                                    </Form.Group>
+                                    <Form.Group as={Col}>
+                                        <Form.Label>Last Name</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            id="last_name"
+                                            name="last_name"
+                                            placeholder="Enter here..."
+                                            value={data.last_name}
+                                            onChange={this.onChange}/>
+                                    </Form.Group>
+                                    <Form.Group as={Col}>
+                                        <Form.Label>Initials</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            id="initials"
+                                            name="initials"
+                                            placeholder="Enter here..."
+                                            value={data.initials}
+                                            onChange={this.onChange}/>
+                                    </Form.Group>
+                                </Form.Row>
+                                <Form.Row>
+                                    <Form.Group as={Col}>
+                                        <Form.Label>Sex</Form.Label>
+                                        <Form.Control as="select" id="sex" name="sex" onChange={this.onChange} value={data.sex}>
+                                            <option value={"F"}>Female</option>
+                                            <option value={"M"}>Male</option>
+                                            <option value={"Other"}>Other</option>
+                                        </Form.Control>
+                                    </Form.Group>
+                                    <Form.Group as={Col}>
+                                        <Form.Label>Date of Birth</Form.Label>
+                                        <DatePicker
+                                            value={data.dob}
+                                            id="dob"
+                                            name="dob"
+                                            dateFormat="MM/DD/YYYY"
+                                            onChange={this.onChangeDob}
+                                        />
+                                    </Form.Group>
+                                </Form.Row>
                             </Col>
                             <Col>
-                                <Form.Group>
-                                    <Form.Label>Initials</Form.Label>
-                                    <Form.Control
-                                        type="text"
-                                        id="initials"
-                                        name="initials"
-                                        placeholder="Initials" />
-                                    {/*error handling*/}
-                                    {/* <Form.Text className="text-muted">
-                                        {errors.email && <InlineError text={errors.email} />}
-                                    </Form.Text> */}
-                                </Form.Group>
+                                <Form.Row>
+                                    <Form.Group as={Col}>
+                                        <Form.Label>Pregnant?</Form.Label>
+                                        <Form.Control as="select" id="pregnant" name="pregnant" onChange={this.onChange} value={data.pregnant}>
+                                            <option value={"true"}>Yes</option>
+                                            <option value={"false"}>No</option>
+                                        </Form.Control>
+                                    </Form.Group>
+                                    <Form.Group as={Col}>
+                                        <Form.Label>Gestational Start Date</Form.Label>
+                                        <DatePicker
+                                            value={data.gestational_start_date}
+                                            id="gestational_start_date"
+                                            name="gestational_start_date"
+                                            dateFormat="MM/DD/YYYY"
+                                            onChange={this.onChangeDateGest}
+                                        />
+                                    </Form.Group>
+                                </Form.Row>
+                                <Form.Row>
+                                    <Form.Group as={Col}>
+                                        <Form.Label>Gestational Age Unit</Form.Label>
+                                        <Form.Control as="select" id="gestational_age_unit" name="gestational_age_unit" onChange={this.onChange} value={data.gestational_age_unit}>
+                                            <option value={"none"}>None</option>
+                                            <option value={"week"}>Week</option>
+                                            <option value={"month"}>Month</option>
+                                        </Form.Control>
+                                    </Form.Group>
+                                    <Form.Group as={Col}>
+                                        <Form.Label>Current Gestational Age</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            id="current_gestational_age"
+                                            name="current_gestational_age"
+                                            placeholder="Enter here..."
+                                            value={data.current_gestational_age}
+                                            onChange={this.onChange}/>
+                                    </Form.Group>
+                                </Form.Row>
                             </Col>
                         </Row>
-                        <Row>
-                            <Col>
-                                <Form.Group>
-                                    <Form.Label>Age</Form.Label>
+
+                            <Form.Row>
+                                <Form.Group as={Col}>
+                                    <Form.Label>Attestation #</Form.Label>
                                     <Form.Control
-                                        type="number"
-                                        id="age"
-                                        name="age"
-                                        placeholder="Age" />
-                                    {/*error handling*/}
-                                    {/* <Form.Text className="text-muted">
-                                        {errors.email && <InlineError text={errors.email} />}
-                                    </Form.Text> */}
+                                        type="text"
+                                        id="attestation_no"
+                                        name="attestation_no"
+                                        placeholder="Enter here..."
+                                        value={data.attestation_no}
+                                        onChange={this.onChange}/>
                                 </Form.Group>
-                            </Col>
-                            <Col>
-                                <Form.Group>
-                                    <Form.Label>Pregnant</Form.Label>
-                                    <Form.Control as="select">
-                                        <option value={'true'}>Yes</option>
-                                        <option value={'False'}>No</option>
-                                    </Form.Control>
-                                    {/* enable his for error handling */}
-                                    {/* <Form.Text className="text-muted">
-                                        {errors.email && <InlineError text={errors.email} />}
-                                    </Form.Text> */}
-                                </Form.Group>
-                            </Col>
-                            <Col>
-                                <Form.Group>
-                                    <Form.Label>Gestational Age</Form.Label>
+                                <Form.Group as={Col}>
+                                    <Form.Label>Village #</Form.Label>
                                     <Form.Control
-                                        type="number"
-                                        id="age"
-                                        name="age"
-                                        placeholder="Age" />
-                                    {/* enable his for error handling */}
-                                    {/* <Form.Text className="text-muted">
-                                        {errors.email && <InlineError text={errors.email} />}
-                                    </Form.Text> */}
+                                        type="text"
+                                        id="village_no"
+                                        name="village_no"
+                                        placeholder="Enter here..."
+                                        value={data.village_no}
+                                        onChange={this.onChange}/>
                                 </Form.Group>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <Button variant="success" as={Link} to="listPatient">
-                                    Create
-                                </Button>
-                            </Col>
+                                <Form.Group as={Col}>
+                                    <Form.Label>Zone #</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        id="zone_no"
+                                        name="zone_no"
+                                        placeholder="Enter here..."
+                                        value={data.zone_no}
+                                        onChange={this.onChange}/>
+                                </Form.Group>
+                            </Form.Row>
+                            <Form.Row>
+                                <Form.Group as={Col}>
+                                    <Form.Label>Household #</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        id="household_no"
+                                        name="household_no"
+                                        placeholder="Enter here..."
+                                        value={data.household_no}
+                                        onChange={this.onChange}/>
+                                </Form.Group>
+                                <Form.Group as={Col}>
+                                    <Form.Label>Block #</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        id="block_no"
+                                        name="block_no"
+                                        placeholder="Enter here..."
+                                        value={data.block_no}
+                                        onChange={this.onChange}/>
+                                </Form.Group>
+                                <Form.Group as={Col}>
+                                    <Form.Label>Tank #</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        id="tank_no"
+                                        name="tank_no"
+                                        placeholder="Enter here..."
+                                        value={data.tank_no}
+                                        onChange={this.onChange}/>
+                                </Form.Group>
+                            </Form.Row>
+                        <Row style={{float: 'right'}}>
+                            <Button primary type="submit" >Create</Button>
                         </Row>
                     </Form>
                 </Container>
