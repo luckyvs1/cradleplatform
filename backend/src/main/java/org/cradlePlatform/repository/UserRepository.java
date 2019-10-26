63a2a86824deef57b6f9b8c9489f81c5ed607f11
lvs1 <lvs1@sfu.ca>
2019-10-25 19:51:59 -0700

    Add query to find by username and password

--- backend/src/main/java/org/cradlePlatform/repository/UserRepository.java
+++ backend/src/main/java/org/cradlePlatform/repository/UserRepository.java
@@ -3,8 +3,11 @@ package org.cradlePlatform.repository;
 import org.cradlePlatform.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, String> {
-
+    @Query("SELECT u FROM User u WHERE u.username = :username and u.password = :password")
+    User findByLoginCredentials(@Param("username") String username, @Param("password") String password);
 }
