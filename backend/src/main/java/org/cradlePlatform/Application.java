470809c8f5b3bdbebd85ba7b6476c9b8edb47da7
lvs1 <lvs1@sfu.ca>
2019-10-27 16:04:49 -0700

    Update cors to use WebMvcConfigurer with variables from application.properties

--- backend/src/main/java/org/cradlePlatform/Application.java
+++ backend/src/main/java/org/cradlePlatform/Application.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation;
+import org.springframework.core.env.Environment;
 
 //Turn off login page for restricting api endpoints
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class})
 public class Application {
+
+	@Autowired
+	private Environment env;
 
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -21,5 +26,19 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				String urls = env.getProperty("cors.urls");
+				CorsRegistration reg = registry.addMapping("/api/**");
+				for(String url: urls.split(",")) {
+					reg.allowedOrigins(url);
+				}
+			}
+		};
 	}
 }
