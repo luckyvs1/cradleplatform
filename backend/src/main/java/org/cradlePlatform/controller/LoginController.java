c9539e11a555f8a3d5947e2cf8fb2685e5c4f353
Gurinder Hans <ghans@sfu.ca>
2019-10-28 04:20:12 -0400

    temp

--- backend/src/main/java/org/cradlePlatform/controller/LoginController.java
+++ backend/src/main/java/org/cradlePlatform/controller/LoginController.java
@@ -8,6 +8,17 @@ package org.cradlePlatform.controller;
 import org.cradlePlatform.model.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+class LoginResp {
+  public LoginResp() {}
+
+  public String getUserId() { return "123"; }
+
+  public String getAccessToken() { return "as7dasd9"; }
+
+  public long getExpiresIn() { return 3600; }
+}
+
 @CrossOrigin(origins = {"http://cmpt373.csil.sfu.ca:8044"})
 @RestController
 public class LoginController {
@@ -20,12 +31,15 @@ public class LoginController {
 
 	@PostMapping("/api/login")
 	@ResponseStatus(HttpStatus.OK)
-	public User logIn(@RequestBody User user) {
-		// TODO: Implement log in
-		User account = new User();
-		account.setUsername("Testuser");
-		account.setPassword("Testpass");
-		return account;
+  public LoginResp logIn(@RequestBody User user) {
+    return new LoginResp();
+
+	// public User logIn(@RequestBody User user) {
+	// 	// TODO: Implement log in
+	// 	User account = new User();
+	// 	account.setUsername("Testuser");
+	// 	account.setPassword("Testpass");
+	// 	return account;
 
 	}
 
