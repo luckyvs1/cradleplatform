24b7ef62c0404e3a8608fa775f5ee03157d7c0f1
lvs1 <lvs1@sfu.ca>
2019-10-29 21:55:08 -0700

    Add process environment variable check for testing host and port config

--- frontend/marsapp/src/api.js
+++ frontend/marsapp/src/api.js
@@ -5,8 +5,19 @@
 
 import axios from "axios/dist/axios"
 
-const host = "cmpt373.csil.sfu.ca";
-const port = "8084";
+let host = "";
+let port = "";
+
+if (process.env.NODE_ENV === 'production') {
+    host = "cmpt373.csil.sfu.ca";
+    port = "8084";
+    console.log("production environment credentials")
+} else {
+    host = "localhost";
+    port = "8080";
+    console.log("local environment credentials")
+}
+
 export default {
     admin:{
         getAdminById: data => {
