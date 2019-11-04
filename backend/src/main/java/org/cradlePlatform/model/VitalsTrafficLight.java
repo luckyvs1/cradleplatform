f6c6ffdf581d7b966e09c659d2296d88baae04e2
lvs1 <lvs1@sfu.ca>
2019-11-03 21:52:49 -0800

    Remove new line markers in string

--- backend/src/main/java/org/cradlePlatform/model/VitalsTrafficLight.java
+++ backend/src/main/java/org/cradlePlatform/model/VitalsTrafficLight.java
@@ -3,48 +3,48 @@ package org.cradlePlatform.model;
 public enum VitalsTrafficLight {
 	Green("Patient is likely healthy",
 			"Continue normal care",
-			"Patient is likely healthy. \n Continue normal care."),
+			"Patient is likely healthy.  Continue normal care."),
 	Yellow_up("Raised BP",
-			"Monitor for preeclampsia. \n Transfer to health centre within 24h.",
-			"This is raised BP this patient may have preeclapsia. \n Action is needed. \n Manage" +
+			"Monitor for preeclampsia.  Transfer to health centre within 24h.",
+			"This is raised BP this patient may have preeclapsia.  Action is needed.  Manage" +
 					" as you would normally e.g. measure urine dipstick, check for signs and symptoms (e.g. headaches, " +
-					"visual disturbance) and act accordingly. \n If in the community transfer when practical " +
+					"visual disturbance) and act accordingly.  If in the community transfer when practical " +
 					"(preferably within 24 hours)."),
 	Yellow_down("Low BP",
 			"Common but assess for infection, bleeding, anaemia, and dehydration.",
 			"This result can be common in pregnant women, however, it may indicate that the mother is " +
-					"developing infection or bleeding. \n The patient needs to be assessed to decide what actions is " +
-					"required. \n If she is well (no bleeding, no signs of infection, feels well) she may have " +
+					"developing infection or bleeding.  The patient needs to be assessed to decide what actions is " +
+					"required.  If she is well (no bleeding, no signs of infection, feels well) she may have " +
 					"anaemia, dehydration, an irregular heart rhythm or endocrine disease or her blood pressure may be " +
-					"low in pregnancy. \n Consider undertaking routine checks for these when possible. \n If she is unwell " +
+					"low in pregnancy.  Consider undertaking routine checks for these when possible.  If she is unwell " +
 					"e.g. vaginal bleeding, fever, discharge, constant abdominal pain or if she feels unwell e.g. " +
-					"feverish, pale, sweaty, breathless \n * Resuscitate as necessary e.g. keep warm, elevate legs \n " +
-					"* Transfer urgently (preferably within 4 hours) \n * If bleeding, uterine massage after delivery " +
+					"feverish, pale, sweaty, breathless  * Resuscitate as necessary e.g. keep warm, elevate legs  " +
+					"* Transfer urgently (preferably within 4 hours)  * If bleeding, uterine massage after delivery " +
 					"of placenta, control of bleeding e.g. misoprostol, oxytocin, depending on what's available" +
-					"\n * If sepsis, consider starting antibiotics if available"),
+					" * If sepsis, consider starting antibiotics if available"),
 	Red_up("Very raised BP",
-			"Urgent action needed. \n Transfer to health centre within 4h. \n Monitor baby.",
-			"This is very raised BP and indicates urgent action is needed. \n Manage as you would " +
-					"normally e.g. measure urine dipstick, check for signs and symptoms and act accordingly \n Give " +
-					"antihypertensives if available e.g. methyidopa, nifedipine, labetolol \n Consider magnesium " +
-					"sulphate (intramuscular) if available. \n If in the community transfer as soon as possible " +
-					"(preferably within 4 hours). \n Monitor the baby. \n If BP remains uncontrolled and gestation " +
+			"Urgent action needed.  Transfer to health centre within 4h.  Monitor baby.",
+			"This is very raised BP and indicates urgent action is needed.  Manage as you would " +
+					"normally e.g. measure urine dipstick, check for signs and symptoms and act accordingly  Give " +
+					"antihypertensives if available e.g. methyidopa, nifedipine, labetolol  Consider magnesium " +
+					"sulphate (intramuscular) if available.  If in the community transfer as soon as possible " +
+					"(preferably within 4 hours).  Monitor the baby.  If BP remains uncontrolled and gestation " +
 					"appropriate, seek senior advice regarding need to deliver."),
 	Red_down("In sever shock",
-			"Urgent action needed. \n Get help and assess mother. \n Immediately transfer to health centre within 1h.",
-			"Community Version \n * Stay calm. Do NOT leave the woman alone \n * Get HELP \n * Assess the " +
-					"mother \n * Is she pale, sweaty, cold, breathing fast, drowsy or confused? \n * Is she unwell e.g. " +
-					"vaginal bleeding, fever, discharge, constant pain? \n Treatment \n * Keep her warm and elevate legs if " +
-					"possible \n * Organise immediate transfer (within 1 hour) \n * If bleeding, uterine massage after delivery " +
-					"of placenta, give medication to contract uterus if available e.g. misoprostil 600mcg orally \n * If sepsis," +
-					" consider starting antibiotics \n Hospital Version \n * Stay calm. Do NOT leave the woman alone \n * Get " +
-					"HELP \n * Assess the mother \n * Is she pale, sweaty, cold, breathing fast, drowsy or confused? \n" +
-					"* Is she unwell e.g. vaginal bleeding, fever, discharge, constant pain? \n Treatment \n * Keep her warm " +
-					"and elevate legs if possible \n * Oxygen \n * IV fluids give quickly through a large bore cannula e.g. 2 " +
-					"litres in first hour \n * Collect blood to test hemoglobin, do an immediate cross-match \n * Catheterise " +
-					"the bladder to monitor input/output \n * Decide on the cause of shock and manage as you would normally \n " +
-					"* If bleeding transfuse blood, give uterotonics such as IV oxytocin, misoprostil or carboprost \n " +
-					"* Consider operative interventions if appropriate and available \n * If severe infection, keep hydrated, " +
+			"Urgent action needed.  Get help and assess mother.  Immediately transfer to health centre within 1h.",
+			"Community Version  * Stay calm. Do NOT leave the woman alone  * Get HELP  * Assess the " +
+					"mother  * Is she pale, sweaty, cold, breathing fast, drowsy or confused?  * Is she unwell e.g. " +
+					"vaginal bleeding, fever, discharge, constant pain?  Treatment  * Keep her warm and elevate legs if " +
+					"possible  * Organise immediate transfer (within 1 hour)  * If bleeding, uterine massage after delivery " +
+					"of placenta, give medication to contract uterus if available e.g. misoprostil 600mcg orally  * If sepsis," +
+					" consider starting antibiotics  Hospital Version  * Stay calm. Do NOT leave the woman alone  * Get " +
+					"HELP  * Assess the mother  * Is she pale, sweaty, cold, breathing fast, drowsy or confused? " +
+					"* Is she unwell e.g. vaginal bleeding, fever, discharge, constant pain?  Treatment  * Keep her warm " +
+					"and elevate legs if possible  * Oxygen  * IV fluids give quickly through a large bore cannula e.g. 2 " +
+					"litres in first hour  * Collect blood to test hemoglobin, do an immediate cross-match  * Catheterise " +
+					"the bladder to monitor input/output  * Decide on the cause of shock and manage as you would normally  " +
+					"* If bleeding transfuse blood, give uterotonics such as IV oxytocin, misoprostil or carboprost  " +
+					"* Consider operative interventions if appropriate and available  * If severe infection, keep hydrated, " +
 					"give IV antibiotics");
 	
 	private final String analysisText;
