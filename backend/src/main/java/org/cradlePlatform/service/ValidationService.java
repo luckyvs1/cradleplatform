37385bafd064e01ce789f176f96c79d82912bf56
lvs1 <lvs1@sfu.ca>
2019-11-06 14:37:34 -0800

    Update validation service error handling

--- backend/src/main/java/org/cradlePlatform/service/ValidationService.java
+++ backend/src/main/java/org/cradlePlatform/service/ValidationService.java
@@ -4,9 +4,11 @@ import org.cradlePlatform.model.Patient;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.cradlePlatform.repository.PatientRepository;
-import org.springframework.util.StringUtils;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.Optional;
+import java.lang.reflect.Field;
+import org.springframework.util.StringUtils;
 
 @Service
 public class ValidationService {
@@ -17,24 +19,41 @@ public class ValidationService {
     // If attestation number is valid digits of length 11 use the current else use empty string
     public String getValidAttestationNo(Patient patient) {
         int ATTESTATION_NO_LENGTH = 11;
-        String emptyString = "";
-        String nonDigitsPattern = "\\D";
+        String defaultAttestationNo = "";
+        String zeroOrMoreDigitsPattern = "[0-9]*";
+        String accessField = "attestation_no";
+        Pattern pattern = Pattern.compile(zeroOrMoreDigitsPattern);
 
-        if(patient.getAttestationNo().isBlank()) {
-            patient.setAttestationNo(emptyString);
+        // Converts null and variations of whitespace to empty string
+        if(StringUtils.isEmpty(patient.getAttestationNo())) {
+            try {
+                Field field = patient.getClass().getDeclaredField(accessField);
+                field.setAccessible(true);
+                field.get(patient);
+                field.set(patient, defaultAttestationNo);
+            } catch (NoSuchFieldException e) {
+                throw new IllegalArgumentException("No such field: " + accessField);
+            } catch (IllegalAccessException e) {
+                throw new IllegalArgumentException("Illegal access for field: " + accessField);
+            }
         }
 
-        String formattedAttestationNo =  StringUtils.trimAllWhitespace(patient.getAttestationNo().replaceAll(nonDigitsPattern, ""));
+        String formattedAttestationNo = StringUtils.trimAllWhitespace(patient.getAttestationNo());
+        Matcher matcher = pattern.matcher(formattedAttestationNo);
 
-        if (!formattedAttestationNo.isBlank() && formattedAttestationNo.length() == ATTESTATION_NO_LENGTH) {
+        if (!matcher.matches()){
+            throw new IllegalArgumentException("Attestation number cannot contain non digits: " + patient.getAttestationNo());
+        }
+
+        if (!StringUtils.isEmpty(formattedAttestationNo) && formattedAttestationNo.length() == ATTESTATION_NO_LENGTH) {
             Optional<Patient> optionalPatient = patientRepository.findUserByAttestationNo(formattedAttestationNo);
             if (optionalPatient.isPresent()) {
                 throw new IllegalArgumentException("Attestation number already exists: " + formattedAttestationNo);
             }
-        } else if (!formattedAttestationNo.isBlank() && formattedAttestationNo.length() != ATTESTATION_NO_LENGTH ) {
-            throw new IllegalArgumentException("Attestation number must be of length eleven: " + formattedAttestationNo);
+        } else if (!StringUtils.isEmpty(formattedAttestationNo) && formattedAttestationNo.length() != ATTESTATION_NO_LENGTH ) {
+            throw new IllegalArgumentException(String.format("Attestation number must be of length %d: %s", ATTESTATION_NO_LENGTH, formattedAttestationNo));
         }
 
-        return formattedAttestationNo;
+        return patient.getAttestationNo();
     }
 }
