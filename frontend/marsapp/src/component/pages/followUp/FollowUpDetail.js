96a3f1b169d32a4305cfe2ccabfe7f59e16c7e3e
Faraz Fazlalizadeh <ffazlali@sfu.ca>
2019-11-10 15:57:26 -0800

    Added Confirm

--- frontend/marsapp/src/component/pages/followUp/FollowUpDetail.js
+++ frontend/marsapp/src/component/pages/followUp/FollowUpDetail.js
@@ -9,19 +9,34 @@ import React from "react";
 import {connect} from "react-redux";
 import FollowUpDetailForm from "../../forms/FollowUpDetailForm";
 import api from "../../../api";
+import ConfirmAlert from "../../utils/ConfirmAlert";
 
 class FollowUpDetail extends React.Component {
-
-    submit = data => {
-        console.log("data",data)
-        api.followUp.addFollowUp(data).then(res=>{
-            console.log(res);
-        })
-        // api.followUp.addFollowUp()
+    state ={
+        isShowConfirm:false,
+        isShowError:false,
+        isShowConfirmMsg:"",
+        isShowErrorMsg:"",
     }
+    submit = data => {
+        console.log("data", data)
+        api.followUp.addFollowUp(data).then(res => {
+            if (res) {
+                this.setState({
+                    ...this.state,
+                    isShowConfirm:true,
+                    isShowConfirmMsg:"NOICE"
+                })
+            }
+        })
+    }
+
     render() {
         return (
+            <div>
             <FollowUpDetailForm submit={this.submit}></FollowUpDetailForm>
+                <ConfirmAlert message={this.state.isShowConfirmMsg} show={this.state.isShowConfirm}></ConfirmAlert>
+            </div>
         );
     }
 }
