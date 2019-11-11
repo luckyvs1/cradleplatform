ec1f1008133c617bb39e8b761848b01d829c91e8
Faraz Fazlalizadeh <ffazlali@sfu.ca>
2019-11-10 17:54:18 -0800

    Added fields to dialog and onchange call

--- frontend/marsapp/src/component/forms/AllFollowUpForm.js
+++ frontend/marsapp/src/component/forms/AllFollowUpForm.js
@@ -4,16 +4,13 @@
  *  Contains the contents and functionality of the FollowUp page.
  */
 
-import React, {useState} from "react";
+import React from "react";
 import TopNavigation from "../navigation/TopNavigation";
-import {Button, Col, Container, Form, Modal, Row} from 'react-bootstrap';
+import {Col, Container, Row, Button, Modal, Form} from 'react-bootstrap';
 import {connect} from "react-redux";
 import FollowUpListTable from "../utils/FollowUpListTable"
-import InlineError from "../messages/InlineError";
 
 class AllFollowUpForm extends React.Component {
-
-
     constructor(props) {
         super(props);
         this.state = {
@@ -26,24 +23,56 @@ class AllFollowUpForm extends React.Component {
                 required: null,
                 treatment: "",
             }],
+            addData: {
+                diagnosis: "",
+                followUpNotes: "",
+                frequency: "",
+                id: null,
+                patientId: "",
+                required: false,
+                treatment: "",
+            },
             showModal: false,
 
-
         };
+        this.handleChange = this.handleChange.bind(this);
+
+    }
+
+    handleChange(event) {
+        console.log(event.target)
+        this.setState({
+            ...this.state,
+            addData: {...this.state.addData, [event.target.name]: event.target.value}
+        });
+    }
+
+
+    addFollowUp = () => {
+        this.setState({
+            ...this.state,
+            showModal: true,
+        })
     }
 
     addFollowUp = () => {
         this.setState({
             ...this.state,
-            showModal:true,
+            showModal: true,
         })
     }
+
+    handleSubmit = (e) => {
+
+        console.log(this.state)
+    }
+
 
     render() {
         const handleClose = () => {
             this.setState({
                 ...this.state,
-                showModal:false,
+                showModal: false,
             })
         };
         return (
@@ -74,14 +103,92 @@ class AllFollowUpForm extends React.Component {
                         <Modal.Title>Add New Follow Up </Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
+                        <Container>
+                            <Row>
+                                <Col>
+                                    <Form.Group>
+                                        <Form.Label>Patient Id</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            id="patient"
+                                            name="patientId"
+                                            onChange={this.handleChange}
+                                            value={this.state.addData.patientId}/>
+                                    </Form.Group>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <Form.Group>
+                                        <Form.Label>Follow Up Note:</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            id="followUpNotes"
+                                            name="followUpNotes"
+                                            onChange={this.handleChange}
+                                            value={this.state.addData.followUpNotes}/>
+                                    </Form.Group>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <Form.Group>
+                                        <Form.Label>Diagnosis</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            id="diagnosis"
+                                            name="diagnosis"
+                                            onChange={this.handleChange}
+                                            value={this.state.addData.diagnosis}/>
+                                    </Form.Group>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <Form.Group>
+                                        <Form.Label>Treatment</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            id="treatment"
+                                            name="treatment"
+                                            onChange={this.handleChange}
+                                            value={this.state.addData.treatment}/>
+                                    </Form.Group>
+                                </Col>
 
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <Form.Group>
+                                        <Form.Label>Frequency</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            id="frequency"
+                                            name="frequency"
+                                            onChange={this.handleChange}
+                                            value={this.state.addData.frequency}/>
+                                    </Form.Group>
+                                </Col>
+                                <Col>
+                                    <Form.Label>Required</Form.Label>
+                                    <Form.Control as="select"
+                                                  name="required"
+                                                  id="required"
+                                                  onChange={this.handleChange}
+                                                  value={this.state.addData.required}>
+                                        <option value={'true'}>Yes</option>
+                                        <option value={'false'}>No</option>
+                                    </Form.Control>
+                                </Col>
+                            </Row>
+                        </Container>
                     </Modal.Body>
                     <Modal.Footer>
                         <Button variant="secondary" onClick={handleClose}>
                             Close
                         </Button>
-                        <Button variant="primary">
-                            Save Changes
+                        <Button variant="primary" onClick={this.handleSubmit}>
+                            Add
                         </Button>
                     </Modal.Footer>
                 </Modal>
