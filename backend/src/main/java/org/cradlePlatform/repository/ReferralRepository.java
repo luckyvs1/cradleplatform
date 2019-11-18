f78ffd7801fdbd78305556a7e685ae593e390b9e
Gurinder Hans <ghans@sfu.ca>
2019-11-17 19:24:03 -0800

    new endpoint, fetch referrals with patient and referrer user info

--- backend/src/main/java/org/cradlePlatform/repository/ReferralRepository.java
+++ backend/src/main/java/org/cradlePlatform/repository/ReferralRepository.java
@@ -1,11 +1,17 @@
 package org.cradlePlatform.repository;
 
 import org.cradlePlatform.model.Referral;
+import org.cradlePlatform.model.ReferralGetWrapper;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReferralRepository extends CrudRepository<Referral, Integer> {
-	Iterable<Referral> findByHealthFacility(String healthFacility);
-	Iterable<Referral> findByReferrerId(String referrerId);
+  Iterable<Referral> findByHealthFacility(String healthFacility);
+  Iterable<Referral> findByReferrerId(String referrerId);
+  @Query("SELECT new org.cradlePlatform.model.ReferralGetWrapper(R, P, UI) FROM Referral R"
+    + " JOIN Patient P ON R.patientId=P.id"
+    + " JOIN UserInfo UI ON R.referrerId=UI.id")
+  Iterable<ReferralGetWrapper> findReferralsWithUserInfo();
 }
