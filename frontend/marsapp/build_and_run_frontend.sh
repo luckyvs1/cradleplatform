5bff5e66b8b269186eff6968c00ae268786ff444
lvs1 <lvs1@sfu.ca>
2019-11-20 07:57:42 -0800

    Update yaml file and add local setup frontend script

---
+++ frontend/marsapp/build_and_run_frontend.sh
@@ -0,0 +1,5 @@
+docker run -it --rm -p 3000:3000 -w /app -v `pwd`/package.json:/app/package.json -v `pwd`/public:/app/public -v `pwd`/src:/app/src -v `pwd`/yarn.lock:/app/yarn.lock node:12.10-slim bash
+
+yarn
+
+yarn start
