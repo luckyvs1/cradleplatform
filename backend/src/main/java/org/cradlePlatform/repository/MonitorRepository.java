24bbc7cd984cc8d83a2db36126a310e8c54ceddd
lvs1 <lvs1@sfu.ca>
2019-11-22 17:35:24 -0800

    Update patient id to be int to match patient id type in MonitorId model and schema

--- backend/src/main/java/org/cradlePlatform/repository/MonitorRepository.java
+++ backend/src/main/java/org/cradlePlatform/repository/MonitorRepository.java
@@ -23,7 +23,7 @@ public interface MonitorRepository extends CrudRepository<Monitor, String> {
             "FROM Patient p INNER JOIN Monitor m " +
             "ON p.id = m.patientId " +
             "WHERE m.vhtId = :vhtId AND m.patientId = :patientId")
-    Patient getPatientByVhtIdAndPatientId(@Param("vhtId") String vhtId, @Param("patientId") String patientId);
+    Patient getPatientByVhtIdAndPatientId(@Param("vhtId") String vhtId, @Param("patientId") int patientId);
 
     @Query( "SELECT r " +
             "FROM Reading r " +
@@ -31,7 +31,7 @@ public interface MonitorRepository extends CrudRepository<Monitor, String> {
                 "SELECT p " +
                 "FROM  Patient p INNER JOIN Monitor m ON p.id = m.patientId " +
                 "WHERE m.vhtId = :vhtId AND m.patientId = :patientId)")
-    Iterable<Reading> findReadingByVhtIdAndPatientId(@Param("vhtId") String vhtId, @Param("patientId") String patientId);
+    Iterable<Reading> findReadingByVhtIdAndPatientId(@Param("vhtId") String vhtId, @Param("patientId") int patientId);
 
     @Query( "SELECT f " +
             "FROM FollowUp f " +
@@ -39,7 +39,7 @@ public interface MonitorRepository extends CrudRepository<Monitor, String> {
                 "SELECT p " +
                 "FROM  Patient p INNER JOIN Monitor m ON p.id = m.patientId " +
                 "WHERE m.vhtId = :vhtId AND m.patientId = :patientId)")
-    Iterable<FollowUp> findFollowUpVhtIdAndPatientId(@Param("vhtId") String vhtId, @Param("patientId") String patientId);
+    Iterable<FollowUp> findFollowUpVhtIdAndPatientId(@Param("vhtId") String vhtId, @Param("patientId") int patientId);
 
     @Transactional
     @Modifying
